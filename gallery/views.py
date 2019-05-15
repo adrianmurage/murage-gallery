d5f9caf2ce6b888e99b79a3dfb92c151b2f2c61a
maina murage <mainamurage21@gmail.com>
2019-05-15 14:32:57 +0300

    Update app urls & views
    Update gallery urls to trigger requests
    and render the view functions related templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Image
 
-# Create your views here.
+
+def gallery_home(request):
+    '''
+    function to render all index page with all images
+    '''
+    images = Image.all_images()
+    return render(request, 'gallery/gallery-home.html', {"images": images})
+
+
+def search_results(request):
+    '''
+    function to handle searching for images by category and rendering the
+    results
+    '''
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'gallery/search.html',
+                      {"message": message, "categories": searched_images})
+    else:
+        message = ""
+        return render(request, 'gallery/search.html', {"message": message})
