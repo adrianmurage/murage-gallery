8e8fd8f60714236412b3b54e507f95f6a9408c7c
maina murage <mainamurage21@gmail.com>
2019-05-15 14:31:43 +0300

    Update models, tests and makemigrations
    update location&category models
    to add save and delete functions and test for them
    update Image model foreign key fields to one word names
    this is to make filtering though them possible

--- gallery/models.py
+++ gallery/models.py
@@ -10,6 +10,18 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
+    def save_location(self):
+        '''
+        method to save location
+        '''
+        self.save()
+
+    def delete_location(self):
+        '''
+        method to delete location
+        '''
+        self.delete()
+
 
 class Category(models.Model):
     '''
@@ -20,6 +32,18 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
+    def save_category(self):
+        '''
+        method to save category
+        '''
+        self.save()
+
+    def delete_category(self):
+        '''
+        method to delete category
+        '''
+        self.delete()
+
 
 class Image(models.Model):
     '''
@@ -28,8 +52,25 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images_storage_path/')
     image_name = models.CharField(max_length=30)
     image_description = models.CharField(max_length=150)
-    image_location = models.ForeignKey(Location)
-    image_category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    @classmethod
+    def all_images(cls):
+        '''
+        method to get all images
+        '''
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        # no test was run for this, kindly review later
+        '''
+        method that searches for images by category
+        '''
+        images = cls.objects.filter(category__category__icontains=search_term)
+        return images
 
     def __str__(self):
         return self.image_name
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,100 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
-# Create your tests here.
+
+class LocationTestClass(TestCase):
+    '''
+    Location model test class
+    '''
+    def setUp(self):
+        self.nairobi = Location(location='nairobi')
+
+    def test_instance(self):
+        '''
+        method to test Location instance
+        '''
+        self.assertTrue(isinstance(self.nairobi, Location))
+
+    def test_save_method(self):
+        '''
+        method to test save location to db functionality
+        '''
+        self.nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        '''
+        method to test delete Location from db functionality
+        '''
+        self.nairobi.save_location()
+        self.nairobi.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
+
+
+class CategoryTestClass(TestCase):
+    '''
+    Category model test class
+    '''
+    def setUp(self):
+        self.cameras = Category(category='cameras')
+
+    def test_instance(self):
+        '''
+        method to test Category instance
+        '''
+        self.assertTrue(isinstance(self.cameras, Category))
+
+    def test_save_method(self):
+        '''
+        method to test save location to db functionality
+        '''
+        self.cameras.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_method(self):
+        '''
+        method to test delete Location from db functionality
+        '''
+        self.cameras.save_category()
+        self.cameras.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+
+
+class ImageTestClass(TestCase):
+    '''
+    Image model test class
+    '''
+    def setUp(self):
+        self.nairobi = Location(location='nairobi')
+        self.nairobi.save_location()
+
+        self.cameras = Category(category='cameras')
+        self.cameras.save_category()
+
+        self.vintage = Image(image='images_storage_path/sssdd.jpg',
+                             image_name='vintage',
+                             image_description='antique camera',
+                             image_location=self.nairobi,
+                             image_category=self.cameras)
+        self.sauda = Image(image='images_storage_path/sssdd.jpg',
+                           image_name='vintage',
+                           image_description='antique camera',
+                           image_location=self.nairobi,
+                           image_category=self.cameras)                             
+
+    def tearDown(self):
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_get_all_images(self):
+        '''
+        method to test if all saved images can be called from db
+        '''
+        self.vintage.save()
+        self.sauda.save()
+        self.assertTrue(len(Image.objects.all()) == 2)
